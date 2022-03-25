8855d4e90c7a6804edbe1363c6403b9b5d923814
admin <khatrivinay1@gmail.com>
2022-03-25 15:33:13 +0530

    privacry

--- js/script.js
+++ js/script.js
@@ -1,28 +1,8 @@
-let stack = []
-
-function toggleModal(event){
-    console.log(event.target)
-    if(event.target.dataset.bsTarget){
-    if(stack[0]==null){
-        stack.pop()
-    }
-    if(stack.length===0){
-          stack.push(bootstrap.Modal.getInstance(document.querySelector(`${event.target.dataset.bsTarget}`)))
-    }
-    else{
-        if(stack.length>=0){
-        stack[0].hide()
-        stack.pop()
-        stack.push(bootstrap.Modal.getInstance(document.querySelector(`${event.target.dataset.bsTarget}`)))}
-        else{
-            stack.pop()
-        }
-    }}
+function toggelDropdown()
+{
+    console.log("hello")
 }
-navtutorialButton = document.querySelector('#tutorial-btn')
-navexampleButton = document.querySelector('#example-btn')
-signupButton = document.querySelector("#signup-btn")
 
-navexampleButton.addEventListener('click', toggleModal)
-navtutorialButton.addEventListener('click', toggleModal)
-signupButton.addEventListener('click', toggleModal)
+dropdownmenu = document.querySelector('ul.dropdown-menu')
+
+dropdownmenu.addEventListener('toggle', toggelDropdown)
